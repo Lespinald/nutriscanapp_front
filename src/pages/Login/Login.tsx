d018b21d086e98ae4db15463ca46b6b665b3b39e
Juan Carreño <jcarrenoar@unal.edu.co>
2024-03-16 09:48:13 -0500

    Fix diseno ingreso y funcion signInWithPopup

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import './login.css'
-// import { useDispatch } from 'react-redux'
-// import { chekingCredentials } from '../../redux/authSlice'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import { firebaseAuth } from '../../firebase'
+
+const googleProvider = new GoogleAuthProvider();
 
 const Login = () => {
 
@@ -14,27 +14,43 @@ const Login = () => {
 
     const HandleLogIn = async (e: React.UIEvent) => {
         e.preventDefault()
-
         if(!address || !password) return;
-
         console.log("pages/Inicio/Login.tsx ~ address:", address, "password:", password);
-
         const result = await signInWithEmailAndPassword(firebaseAuth, address, password);
         console.log("pages/Inicio/Login.tsx ~ result:", result);
     }
 
+    const HandleGoogle = async(e: React.UIEvent) => {
+        e.preventDefault()
+        try{
+            const result = await signInWithPopup(firebaseAuth,googleProvider)
+            const {displayName,email,uid,photoURL} = result?.user;        
+            console.log("🚀 ~ HandleGoogle ~ displayName,email,uid,photoURL:", displayName,email,uid,photoURL)
+            const { creationTime } = result?.user?.metadata;
+            console.log("🚀 ~ HandleGoogle ~ creationTime:", creationTime)
+        }catch (error){
+            console.log("🚀 ~ HandleGoogle ~ error:", error)
+        }
+    }
+
 
     return (
-        <div>
-        <h1>BIENVENIDO DE NUEVO</h1>
-        <form>
-            <input type='email' id='email' placeholder='Correo' value={address} onChange={e => setAddress(e.currentTarget.value)}></input>
-            <input type='password' id='password' placeholder='Contraseña' value={password} onChange={e => setPassword(e.currentTarget.value)}></input>
-            <label>¿Sin cuenta aún? <a>Ingresa aquí</a></label>
-            <button onClick={HandleLogIn}>Iniciar sesión</button>
-        </form>
-        <img src='/WhiteBGLogo.png' alt='Logo'></img>
-        <label>NutriScan</label>
+        <div className='fondoLogin'>
+            <h1 className='loginTitle'>BIENVENIDO</h1>
+            <form style={{display:'flex',flexDirection:'column'}}>
+                <input autoComplete='true' type='email' id='email' placeholder='Correo' value={address} onChange={e => setAddress(e.currentTarget.value)}></input>
+                <input autoComplete='true' type='password' id='password' placeholder='Contraseña' value={password} onChange={e => setPassword(e.currentTarget.value)}></input>
+                <button onClick={HandleLogIn} className='button_logIn'>
+                    Iniciar sesión
+                </button>
+                <div className='fondoLogin-line'></div>
+                <button onClick={HandleGoogle} className='button_google'>
+                    <img src='\Login\GoogleIcon.png'></img>
+                    <p>Continuar con Google</p>
+                </button>
+            </form>
+            <a  className='fondoLogin_label' href='/registro'>¿No tienes cuenta? Ingresa aquí</a>
+            <img src='\Login\nutriscanLogo.png' alt='Logo' style={{height:'24svh',maxHeight:'200px',aspectRatio:' 1.3/1'}}></img>
         </div>
     )
 }
