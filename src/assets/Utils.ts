35911aab191aa348dd9ae0a643f35a118a0afae7
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2024-04-01 23:26:31 -0500

    utils otrografia

--- src/assets/Utils.ts
+++ src/assets/Utils.ts
@@ -1,9 +1,12 @@
-export function GetVierportWidth(): number{
+export function GetViewportWidth(): number{
   return document.documentElement.clientWidth;
 }
-export function GetVierportHeight(): number{
+export function GetViewportHeight(): number{
   return document.documentElement.clientHeight;
 }
 export function GetAspectRatio(): number{
   return document.documentElement.clientWidth/document.documentElement.clientHeight;
+}
+export function IsMobile(): boolean{
+  return (GetViewportWidth() < 700 || GetAspectRatio() <= (3/4))
 }
