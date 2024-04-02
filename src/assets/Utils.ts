ee8d9754d7c3fe7d1d101e4ece21423ce033501d
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2024-04-01 23:21:10 -0500

    update estilos home page desktop

---
+++ src/assets/Utils.ts
@@ -0,0 +1,9 @@
+export function GetVierportWidth(): number{
+  return document.documentElement.clientWidth;
+}
+export function GetVierportHeight(): number{
+  return document.documentElement.clientHeight;
+}
+export function GetAspectRatio(): number{
+  return document.documentElement.clientWidth/document.documentElement.clientHeight;
+}
---
+++ src/pages/Home/Servicio.tsx
@@ -0,0 +1,11 @@
+import style from "./styles/Servicio.module.css"
+
+const Servicio = () => {
+  return (
+    <div className="main">
+
+    </div>
+  );
+}
+
+export default Servicio;
