71af9c0ad60acb84729a4fabcaee0f3a38448744
Juan Carreño <jcarrenoar@unal.edu.co>
2024-04-28 21:31:33 -0500

    Fix texto intro app

--- src/pages/Home/Marketing.tsx
+++ src/pages/Home/Marketing.tsx
@@ -7,10 +7,13 @@ const Marketing = forwardRef<HTMLDivElement>(({}, ref) => {
       <div className={style.left}>
         <img src="/Home/Marketing/logoColor1.png" alt="logo Nutriscan" className={style.logo}/>
         <p className={style.quote}>
-          - "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna
-          aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat.""
+          "¡Bienvenido a nuestra aplicación de nutrición, tu compañero
+          de viaje hacia un estilo de vida más saludable! Con nuestra
+          aplicación, descubrirás cómo hacer elecciones alimenticias inteligentes
+          que nutrirán tu cuerpo y potenciarán tu bienestar. Desde consejos de 
+          expertos hasta recetas deliciosas y planes de comidas personalizados, estamos
+          aquí para ayudarte en cada paso del camino hacia una vida más equilibrada 
+          y vibrante. ¡Prepárate para sentirte mejor que nunca!"
         </p>
       </div>
       <div className={style.right}>
