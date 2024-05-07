71bb434649b8294be0798f651b0c8d72fa03866c
Juan Carreño <jcarrenoar@unal.edu.co>
2024-05-07 18:19:09 -0500

    Add 404, loggeo automatico

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home/Home';
 import Scan from './pages/Scan/Scan';
@@ -13,36 +13,54 @@ import MenuTienda from './pages/Tienda/MenuTienda';
 import PublicidadTienda from './pages/Publicidad/PublicidadTienda';
 import Inicio from './pages/Home/Inicio';
 import InicioLoggin from './pages/InicioLoggin/InicioLoggin';
+import NotFound from './pages/404/NotFound';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+  const navigate = useNavigate(); // Aquí usamos useNavigate
   
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      // Indicar que se debe redirigir al usuario
+      RedirecLoggeoAutomatico()
+    }
+  }, []);
+
+  const RedirecLoggeoAutomatico = () => {
+    // Redirigir al usuario a la página de inicio de sesión usando navigate
+    navigate('/login', { replace: true });
+  }
 
   return (
-    <BrowserRouter>
-      <Routes>
-        
-        <Route element={<Layout/>}>
-          <Route path=':section?' element={<Home/>}/>
-          <Route path='/pago/:info' element={<Checkout/>}/>
-          <Route path='/responseFactura' element={<RecivePasarela/>}/>
-          <Route path='/publicidad' element={<PublicidadTienda/>}/>
-        </Route>
-        <Route path='Registro' element={<Registro/>}/>
-        <Route path='Login' element={<Login/>}/>
-        
+    <Routes>
+      <Route element={<Layout/>}>
+        <Route path=':section?' element={<Home/>}/>
+        <Route path='/Scan' element={<Scan/>}/>
+        <Route path='/pago/:info' element={<Checkout/>}/>
+        <Route path='/responseFactura' element={<RecivePasarela/>}/>
+        <Route path='/publicidad' element={<PublicidadTienda/>}/>
+      </Route>
+      <Route path='Registro' element={<Registro/>}/>
+      <Route path='Login' element={<Login/>}/>
+      
 
-        <Route path="Home" element={<Navigate to="/" replace/>} />
+      <Route path="Home" element={<Navigate to="/" replace/>} />
 
-        <Route path='app' element={<AppLayout/>}>
-          <Route path='Home' element={<InicioLoggin/>}/>
-          <Route path='Scan' element={<Scan/>}/>
-          <Route path='Perfil' element={<MenuPerfil/>}/>
-          <Route path='EditPerfil' element={<FormPerfil/>}/>
-          <Route path='Tienda' element={<MenuTienda/>}/>
-          <Route path='Publicidad' element={<PublicidadTienda/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <Route path='app' element={<AppLayout/>}>
+        <Route path='Home' element={<InicioLoggin/>}/>
+        <Route path='Scan' element={<Scan/>}/>
+        <Route path='Perfil' element={<MenuPerfil/>}/>
+        <Route path='EditPerfil' element={<FormPerfil/>}/>
+        <Route path='Tienda' element={<MenuTienda/>}/>
+        <Route path='Publicidad' element={<PublicidadTienda/>}/>
+      </Route>
+
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>        
   );
 }
 
