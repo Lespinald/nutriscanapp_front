7e5a143fb646ce74149a0f56a52bf228337b56a1
Juancho Rodriguez <juancho.chiquiza12@gmail.com>
2024-05-19 22:09:31 -0500

    fixed remove /public

--- src/pages/Pasarela/Logo.tsx
+++ src/pages/Pasarela/Logo.tsx
@@ -8,7 +8,7 @@ interface Props{
 const Logo = ({children,color}:Props) => {
   return (
     <div className={style.logo}>
-        <img className={style.hoja} src='/public/Pasarela/HojaLogo.png' alt='Imagen Hoja'></img>
+        <img className={style.hoja} src='/Pasarela/HojaLogo.png' alt='Imagen Hoja'></img>
         <div className={style.borde} style={{border:`10px solid ${color}`}}>
             <div className={style.letra} style={{color:`${color}`}}>
                 {children}
