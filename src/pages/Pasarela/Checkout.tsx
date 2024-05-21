a3d381e86cc4784223240564a1567b1ffef1280b
Lespinald <55605978+Lespinald@users.noreply.github.com>
2024-05-20 21:52:56 -0500

    Cambio de suscricpión en el checkout

--- src/pages/Pasarela/Checkout.tsx
+++ src/pages/Pasarela/Checkout.tsx
@@ -5,13 +5,13 @@ import { IsMobile } from '../../assets/Utils'
 import Stripe from 'stripe'
 import { useSelector } from 'react-redux'
 
-
 const Checkout = () => {
   const info = useParams().info
   
   const infoUser = useSelector((state:any) => state.auth.infoUsuario)
-  const [nombre, setNombre] = useState(infoUser?.nombre??'')
-  const [correo, setCorreo] = useState(infoUser?.correo??'')
+  const [nombre, setNombre] = useState(infoUser?.nombre ?? '')
+  const [correo, setCorreo] = useState(infoUser?.correo ?? '')
+  const [selectedSubscription, setSelectedSubscription] = useState(info === 'market' ? '50000' : '7000') // Estado para manejar la suscripción seleccionada
 
   const stripe = new Stripe('sk_test_51PANts06rYdnPSbTI5dy2sGkBMm8ipqD35dgnZVhlGrO0EwdMPhLlHsvw0FqP7VdLbw7N1iSjP4XM9KH4NZSGXzE00pLlIHxUM')
 
@@ -22,19 +22,28 @@ const Checkout = () => {
     "Información nutricional de calidad"
   ];
 
-    // Obtiene la fecha actual
-    const expiraDate = new Date();
-    expiraDate.setDate(expiraDate.getDate()+7);
+  // Obtiene la fecha actual
+  const expiraDate = new Date()
+  expiraDate.setDate(expiraDate.getDate() + 7)
 
-    // Formatea la fecha en formato DD/MM/YYYY
-    const formattedDate = expiraDate.toLocaleDateString('es-ES');
-    const handleClickPagar = async () => {
+  // Formatea la fecha en formato DD/MM/YYYY
+  const formattedDate = expiraDate.toLocaleDateString('es-ES')
+
+  const handleClickPagar = async () => {
+    // IDs de precios para las suscripciones
+    const priceId = selectedSubscription === '7000' 
+      ? 'price_1PAOQS06rYdnPSbTQIx40jAo' 
+      : selectedSubscription === '50000' 
+      ? 'price_1PI1Pt06rYdnPSbTEpyfODxv'
+      : null;
+
+    if (priceId) {
       const session = await stripe.checkout.sessions.create({
         success_url: `http://localhost:5173/responseFactura?nombre=${infoUser?.nombre}&id=${infoUser?.uid}`, 
         cancel_url: 'http://localhost:5173',
         line_items: [
           {
-            price: 'price_1PAOQS06rYdnPSbTQIx40jAo',
+            price: priceId,
             quantity: 1,
           },
         ],
@@ -43,48 +52,66 @@ const Checkout = () => {
       if (session.url) {
         window.location.href = session.url;
       }
-    } 
+    } else {
+      console.error('ID de precio no encontrado');
+    }
+  }
+
+  const handleSubscriptionChange = (event) => {
+    setSelectedSubscription(event.target.value)
+  }
+
   return (
     <div className={style.checkoutFondo}>
       <div className={style.box}>
         <label>Nombre completo</label>
         <input 
-        type='text' 
-        placeholder='Ingrese su nombre' 
-        value={nombre} 
-        onChange={(e) => setNombre(e.target.value)}></input>
+          type='text' 
+          placeholder='Ingrese su nombre' 
+          value={nombre} 
+          onChange={(e) => setNombre(e.target.value)} 
+        />
         <label>Correo</label>
         <input 
-        type='text' 
-        placeholder='Ingrese su correo' 
-        value={correo} onChange={(e) => setCorreo(e.target.value)}></input>
+          type='text' 
+          placeholder='Ingrese su correo' 
+          value={correo} 
+          onChange={(e) => setCorreo(e.target.value)} 
+        />
         <video 
-        className={style.video} 
-        src="video.mp4" 
-        poster="/src/assets/PosterVideo.png" 
-        preload="metadata" controls></video>
+          className={style.video} 
+          src="video.mp4" 
+          poster="/src/assets/PosterVideo.png" 
+          preload="metadata" 
+          controls 
+        />
       </div>
       <div className={style.recibo}>
         <h1 className={style.titulo} style={IsMobile() ? {} : {display:'none'}}>LISTO PARA SUSCRIBIRTE</h1>
         <p className={style.a} style={IsMobile() ? {display:'none'} : {}}>Nombre:</p>
         <p className={style.a} style={IsMobile() ? {display:'none'} : {}}>{nombre}</p>
         <p className={style.a}>Suscripción:</p>
-        <p className={style.a}>NUTRISCAN {info === 'market' ? 'TIENDA' : 'PLUS'}</p>
+        <select value={selectedSubscription} onChange={handleSubscriptionChange} className={style.subscriptionSelect}>
+          <option value="7000">NUTRISCAN PLUS $7000</option>
+          <option value="50000">NUTRISCAN TIENDA $50000</option>
+        </select>
         <p className={style.a}>HASTA:</p>
         <p className={style.a}>{formattedDate}</p>
         <ul className={style.listado}>
-            {condicionesTienda.map((condicion,index) => (
-                <li key={index}>{condicion}</li>
-            ))}
+          {condicionesTienda.map((condicion, index) => (
+            <li key={index}>{condicion}</li>
+          ))}
         </ul>
         <div className={style.linea}></div>
         <p className={style.a}>TOTAL</p>
-        <p className={style.fill}>$ {info === 'market' ? '50.000' : '7.000'}</p>
-        <button className={style.buttonPagar} onClick={handleClickPagar} >Pagar</button>
+        <p className={style.fill}>$ {selectedSubscription}</p>
+        <button className={style.buttonPagar} onClick={handleClickPagar}>Pagar</button>
       </div>
     </div>
   )
 }
 
 export default Checkout
+
+
 
